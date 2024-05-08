5e4685da4d7516f8abff96f4c55d0531ee83dd6f
aezzahir <aero.glowsky@gmail.com>
2024-05-08 22:23:28 +0100

    1. FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,15 +13,15 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """put key: value in cache dict FIFO"""
-        if key and item:
-            self.cache_data[key] = item
-        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
             key_to_pop = list(self.cache_data.keys())[0]
             for key in self.cache_data.keys():
                 if key < key_to_pop:
                     key_to_pop = key
             self.cache_data.pop(key_to_pop)
             print("DISCARD: ", key_to_pop)
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """get key: value from cache dict"""
