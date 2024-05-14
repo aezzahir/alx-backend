9ccb45489c65bba1ec26a000a0c70dd4c3ce71d2
aezzahir <aero.glowsky@gmail.com>
2024-05-14 22:06:51 +0100

    add all

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,13 +21,15 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ Determine the best match with our supported languages. """
+    if 'locale' in request.args and request.args['locale'] in Config.LANGUAGES:
+        return request.args['locale']
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
 def index():
     """Render the index.html template"""
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
